128d16cf742bbf0c99185ffbf61be513fcb53521
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-05-22 09:29:33 -0300

    Subindo novos arquivos .py streaming twitter

--- streaming-spark/listener_twitter.py
+++ streaming-spark/listener_twitter.py
@@ -3,7 +3,7 @@ import tweepy
 import time
 
 HOST = 'localhost'
-PORT = 9009
+PORT = 9000
 
 
 s = socket.socket()
@@ -11,18 +11,18 @@ s.bind((HOST, PORT))
 print(f"Aguardando conexão na porta: {PORT}")
 
 s.listen(5)
-conn, address = s.accept()
+connection, address = s.accept()
 print(f"Recebendo solicitação de {address}")
 
 token = 'AAAAAAAAAAAAAAAAAAAAAGyzlQEAAAAAaMWf55m0Dzy%2Fugc%2BGnVxP6Pp6h4%3DykHqTs8KhSU8rBIy6ah0z6nzSCG9ZQsWAVO3ZDm6LJHXoPNsB1'
-keyword = 'Biblioteconomia' 
+keyword = 'biblioteconomia' 
 
 class GetTweets(tweepy.StreamingClient):
     
     def on_tweet(self,tweet):
         print(tweet.text)
         print('='*50)
-        connection.send(tweet.text.encode('latin1','ignore'))
+        connection.send(tweet.text.encode('utf-8','ignore'))
         
 printer = GetTweets(token)
 printer.add_rules(tweepy.StreamRule(keyword))
